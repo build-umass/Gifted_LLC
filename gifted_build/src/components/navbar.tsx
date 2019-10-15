df3489a8d887ebe315aef6a3109f07181cefbb99
Daniel Walsh <djwalsh@umass.edu>
2019-10-15 18:08:02 -0400

    Changed navbar button labels

--- gifted_build/src/components/navbar.tsx
+++ gifted_build/src/components/navbar.tsx
@@ -6,12 +6,12 @@ class NavBar extends React.Component<{}, {}> {
         return (
             <div className='container'>
                 <ul id='navbar'>
-                    <a href="/"><li id='nav-name' className="nav-label">    Gifted-Ny       </li></a>
-                    <a href="/2"><li className="nav-label">                 Second Page     </li></a>
-                    <a href="/"><li className="nav-label">                  Element 1       </li></a>
-                    <a href="/"><li className="nav-label">                  Element 2       </li></a>
-                    <a href="/"><li className="nav-label">                  Steven          </li></a>
-                    <a href="/"><li className="nav-label">                  is cool         </li></a>
+                    <a href="/"><li id='nav-name' className="nav-label">Gifted-Ny</li></a>
+                    <a href="/2"><li className="nav-label">Gifted</li></a>
+                    <a href="/"><li className="nav-label">Home</li></a>
+                    <a href="/"><li className="nav-label">Shop</li></a>
+                    <a href="/"><li className="nav-label">About</li></a>
+                    <a href="/"><li className="nav-label">Cart</li></a>
                 </ul>
             </div>
             )
