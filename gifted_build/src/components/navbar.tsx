d6947a8e56fda3613f01502df15d7c9078cee249
Daniel Walsh <djwalsh@umass.edu>
2019-10-15 19:29:48 -0400

    updated navbar.

--- gifted_build/src/components/navbar.tsx
+++ gifted_build/src/components/navbar.tsx
@@ -9,7 +9,7 @@ class NavBar extends React.Component<{}, {}> {
                     <a href="/"><li id='nav-name' className="nav-label button-left">Gifted-Ny</li></a>
                     <a href="/2"><li className="nav-label button-left">Gifted</li></a>
                     <a href="/"><li className="nav-label button-right">Cart</li></a>
-                    <a href="/"><li className="nav-label button-right">About</li></a>
+                    <a href="/about"><li className="nav-label button-right">About</li></a>
                     <a href="/"><li className="nav-label button-right">Shop</li></a>
                     <a href="/"><li className="nav-label button-right">Home</li></a>
                 </ul>
