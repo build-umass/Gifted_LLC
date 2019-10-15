960152002f62873689caa10a2ed92d0a268590ed
Daniel Walsh <djwalsh@umass.edu>
2019-10-15 19:03:28 -0400

    Uploaded images and edited Navbar.

--- gifted_build/src/components/navbar.tsx
+++ gifted_build/src/components/navbar.tsx
@@ -8,10 +8,10 @@ class NavBar extends React.Component<{}, {}> {
                 <ul id='navbar'>
                     <a href="/"><li id='nav-name' className="nav-label button-left">Gifted-Ny</li></a>
                     <a href="/2"><li className="nav-label button-left">Gifted</li></a>
-                    <a href="/"><li className="nav-label button-right">Home</li></a>
-                    <a href="/"><li className="nav-label button-right">Shop</li></a>
-                    <a href="/"><li className="nav-label button-right">About</li></a>
                     <a href="/"><li className="nav-label button-right">Cart</li></a>
+                    <a href="/"><li className="nav-label button-right">About</li></a>
+                    <a href="/"><li className="nav-label button-right">Shop</li></a>
+                    <a href="/"><li className="nav-label button-right">Home</li></a>
                 </ul>
             </div>
             )
