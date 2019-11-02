0e75876bc3f506f1834cf9fecaed2d507b4b7f22
Vedant Roy <vroy101@gmail.com>
2019-11-02 15:18:27 -0400

    Add /products route

--- gifted_build/src/server/routes/test.js
+++ gifted_build/src/server/routes/test.js
@@ -1,5 +1,28 @@
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      user: 'build',
+      host: 'localhost',
+      database: 'api',
+      password: 'password',
+      port: 5432
+    },
+    pool: { min: 0, max: 5 } // no logic to these numbers
+}); // Without this semicolon the program will break b/c Javascript has a silly parser.
 const express = require('express');
 const router = express.Router();
+
+router.get('/products/', (req, res) => {
+    knex('products')
+        .select()
+        .then(rows => {
+            console.log(rows)
+            res.json(rows)
+            //res.send("test")
+            //res.end(rows)
+        })
+        .catch(err => res.end({err: err}))
+})
 
 router.get('/', function(req, res) {
     res.send("I'm a little teacup");
