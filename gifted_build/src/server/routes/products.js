7e84b1a0e1cced3215f57861c61cc132e7eca62b
Vedant Roy <vroy101@gmail.com>
2019-11-02 19:42:29 -0400

    Put database config in separate file

--- gifted_build/src/server/routes/products.js
+++ gifted_build/src/server/routes/products.js
@@ -1,14 +1,4 @@
-const knex = require('knex')({
-    client: 'pg',
-    connection: {
-      user: 'build',
-      host: 'localhost',
-      database: 'api',
-      password: 'password',
-      port: 5432
-    },
-    pool: { min: 0, max: 5 } // no logic to these numbers
-}); // Without this semicolon the program will break b/c Javascript has a silly parser.
+const { knex } = require('../database')
 const express = require('express');
 const router = express.Router();
 
